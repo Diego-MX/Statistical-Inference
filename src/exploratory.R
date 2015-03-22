bdb578527b0a6c74c042c59e025abcacd292ba2b
Diego <dvillamil.github@gmail.com>
2015-03-22 16:31:35 -0600

    Update Toothgrowth.

--- src/exploratory.R
+++ src/exploratory.R
@@ -1,8 +1,19 @@
 # Diego, March 2015
 
+library(dplyr)
+library(reshape2)
 library(datasets)
+library(graphics)
 data(ToothGrowth)
 
-toothData <- ToothGrowth %>% mutate(ID = rep(1:10,6)) %>%
-    dcast(ToothGrowth, ID ~ dose + supp, value.var="len")
+toothData <- mutate(ToothGrowth, ID = rep(1:10,6))
+
+png("../output/ToothGrowth.png")
+coplot(len ~ dose | supp, data = ToothGrowth,
+    panel = panel.smooth, xlab = paste("ToothGrowth data:",
+    "length vs dose, given type of supplement"))
+nil <- dev.off()
+
+
+# Tests
 
